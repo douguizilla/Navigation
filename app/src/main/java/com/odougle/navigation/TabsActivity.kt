74e3662ed2c30e65400c40697584ed9c6fda36df
douguizilla <douglasgommez@gmail.com>
2021-08-23 15:26:07 -0300

    created TabsPagerAdapter.kt and import it in TabsActivity.kt

--- app/src/main/java/com/odougle/navigation/TabsActivity.kt
+++ app/src/main/java/com/odougle/navigation/TabsActivity.kt
@@ -2,15 +2,24 @@ package com.odougle.navigation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.odougle.navigation.adapters.TabsPagerAdapter
 import com.odougle.navigation.databinding.ActivityTabsBinding
+import com.odougle.navigation.databinding.ToolbarBinding
 
 class TabsActivity : AppCompatActivity() {
     private val binding: ActivityTabsBinding by lazy {
         ActivityTabsBinding.inflate(layoutInflater)
     }
+    private val toolbar by lazy{ ToolbarBinding.inflate(layoutInflater) }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        setSupportActionBar(toolbar.toolbar)
+        val pagerAdapter = TabsPagerAdapter(this, supportFragmentManager)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        binding.viewPager.adapter = pagerAdapter
+        binding.tabLayout.setupWithViewPager(binding.viewPager)
     }
 }
---
+++ app/src/main/java/com/odougle/navigation/adapters/TabsPagerAdapter.kt
@@ -0,0 +1,30 @@
+package com.odougle.navigation.adapters
+
+import android.content.Context
+import android.content.res.TypedArray
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import com.odougle.navigation.R
+
+class TabsPagerAdapter(
+    context: Context,
+    fm: FragmentManager
+) : FragmentPagerAdapter(fm) {
+
+    private val tabTitles: Array<String> = context.resources.getStringArray(R.array.sections)
+    private val bgColors: TypedArray = context.resources.obtainTypedArray(R.array.bg_colors)
+    private val textColors: TypedArray = context.resources.obtainTypedArray(R.array.text_colors)
+
+    override fun getCount() = 3
+
+    override fun getItem(position: Int) = SecondLevelFragment.newInstance(
+        tabTitles[position],
+        bgColors.getColor(position, 0),
+        textColors.getColor(position, 0)
+    )
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        return tabTitles[position]
+    }
+}
