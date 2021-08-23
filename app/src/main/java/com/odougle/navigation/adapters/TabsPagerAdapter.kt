614cec18cc852ad210502de7e5e019c0de04e7e0
douguizilla <douglasgommez@gmail.com>
2021-08-23 15:44:36 -0300

    exchanged AndroidManifest.xml

--- app/src/main/java/com/odougle/navigation/adapters/TabsPagerAdapter.kt
+++ app/src/main/java/com/odougle/navigation/adapters/TabsPagerAdapter.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
 import com.odougle.navigation.R
+import com.odougle.navigation.fragments.SecondLevelFragment
 
 class TabsPagerAdapter(
     context: Context,
