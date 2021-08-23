7739d0aadd7798a8e50915381ad4fe0d18d6d407
douguizilla <douglasgommez@gmail.com>
2021-08-23 16:53:00 -0300

    implemented PagerActivity.kt

--- app/src/main/java/com/odougle/navigation/PagerActivity.kt
+++ app/src/main/java/com/odougle/navigation/PagerActivity.kt
@@ -2,10 +2,25 @@ package com.odougle.navigation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.odougle.navigation.adapters.TabsPagerAdapter
+import com.odougle.navigation.databinding.ActivityPagerBinding
+import com.odougle.navigation.databinding.ToolbarBinding
 
 class PagerActivity : AppCompatActivity() {
+    private val binding: ActivityPagerBinding by lazy {
+        ActivityPagerBinding.inflate(layoutInflater)
+    }
+    private val toolbarBinding: ToolbarBinding by lazy {
+        ToolbarBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_pager)
+        setContentView(binding.root)
+        setSupportActionBar(toolbarBinding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        val tabsPagerAdapter = TabsPagerAdapter(this, supportFragmentManager)
+        binding.viewPager.adapter = tabsPagerAdapter
+
     }
 }
